42e1c629008afedebf0635853590267986f3effc
fury-cmd <84401549+fury-cmd@users.noreply.github.com>
2021-05-18 19:32:08 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,139 @@
+// server.js
+// where your node app starts
+
+// we've started you off with Express (https://expressjs.com/)
+// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+const express = require('express');
+const app = express();
+
+
+const http = require('http');
+    app.get("/", (request, response) => {
+    console.log(` az önce pinglenmedi. Sonra ponglanmadı... ya da başka bir şeyler olmadı.`);
+    response.sendStatus(200);
+    });
+    app.listen(process.env.PORT);
+    setInterval(() => {
+    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+    }, 280000);
+
+
+// our default array of dreams
+const dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+];
+
+
+
+
+// use discord modules
+const { MessageEmbed, Client, Guild, User, Collection, Permissions } = require("discord.js"),
+      Discord = require("discord.js"),
+      data = require("./ayarlar.json"),
+      db = require("quick.db"),
+      priv = data.priv;
+
+// other
+const tts = require("google-tts-api")
+const fs = require("fs")
+const { promisify } = require("util");
+const readdir = promisify(fs.readdir);
+// defining things from modules
+const client = new Discord.Client();
+client.ayarlar = data
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) console.error(err);
+  console.log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    console.log(`Yüklenen komut: ${props.help.name}`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+require('./util/eventLoader')(client);
+
+
+
+ 
+client.on('guildCreate', sunucu => {
+if(sunucu.memberCount < 20) return sunucu.leave()
+})
+
+
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.elevation = message => {
+  if (!message.guild) return;
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id == client.ayarlar.sahip) permlvl = 4;
+  return permlvl;
+};
+
+client.login(data.token)
+.then(async function(response) {
+  await console.log("Bot hazır: " + `https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=2080374975&scope=bot`)
+})
+
+
+
+
+
